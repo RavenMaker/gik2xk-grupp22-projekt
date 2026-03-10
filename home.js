9efea380ac97533818d7647d4b9e4d97815a6372
Evin Aslan <h24evias@du.se>
2026-03-10 17:16:00 +0100

    förbättrat server

---
+++ home.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+import CategoryPreview from "../components/CategoryPreview";
+import Footer from "../components/Footer";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      
+      <CategoryPreview />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
