dcef87ba7548e785e6128d82c072aec15d316407
Evin Aslan <h24evias@du.se>
2026-03-12 12:24:26 +0100

    ..

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { connectDB } = require("./config/db");
+
+// Starta anslutningen
+connectDB();
 
 const app = express();
 
